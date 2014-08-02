dd4f2c63cf4b7c7e82901a1357a924c0e89a8ae4
crimsonronin <josh.stuart@zoopcommerce.com>
2014-08-02 19:40:07 +1000

    Fixed duplicating config.

--- tests/test.application.config.php
+++ tests/test.application.config.php
@@ -15,7 +15,6 @@ return [
     ],
     'module_listener_options' => [
         'config_glob_paths' => [
-            __DIR__ . '/../config/module.config.php',
             __DIR__ . '/test.module.config.php',
         ],
     ],
