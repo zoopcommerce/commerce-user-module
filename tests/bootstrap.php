7a7059cbdc9aabacec6f0a8b7b63ffb643e98d2e
crimsonronin <josh.stuart@zoopcommerce.com>
2014-09-13 16:41:10 +1000

    Updated shippable unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,17 @@
 <?php
 
-putenv("SERVER_TYPE=test");
+putenv('SERVER_TYPE=test');
 
-$applicationRoot = '/../';
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('memory_limit', '256M');
 
-chdir(__DIR__ . $applicationRoot);
+chdir(__DIR__ . '/../');
 
-$loader = require_once('vendor/autoload.php');
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+if (!isset($loader)) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+}
 
 $loader->add('Zoop\User\Test', __DIR__);
